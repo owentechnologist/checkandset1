d189d5e604a1d4669c8b098a187e3243e418a1f1
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2023-07-03 03:29:34 -0500

    Updated the example to be simpler It currently expects the value stored to be a numeric

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using NRedisStack;
 using NRedisStack.RedisStackCommands;
 using StackExchange.Redis;
+using System.Diagnostics;
 
 namespace checkandset{
     public class RedisJSONCheckAndSetLua{
+        static string luaString = "local workingVersionID = redis.call('JSON.RESP', KEYS[1], '$.versionID') if not workingVersionID then workingVersionID = 1 else if workingVersionID[1]..'' == ARGV[2]..'' then workingVersionID = ARGV[2] + 1 else if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID) end end if redis.call('EXISTS', KEYS[1]) == 0 then redis.call('JSON.SET', KEYS[1], '$', '{\"val\": ' .. ARGV[1] .. '}') end redis.call('JSON.SET', KEYS[1], '$.versionID', workingVersionID) redis.call('JSON.SET', KEYS[1], '$.val', ARGV[1]) if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID)";
 
         public static void Main(string[] args){
-            var redisOptions = ConfigurationOptions.Parse("redis-14154.homelab.local:14154"); // edit to suit
-            //redisOptions.Password = "yourPassword"; //for ACRE connections use key
+            string redisEndpoint = "jsonme.centralus.redisenterprise.cache.azure.net:10000";
+            //redisEndpoint = "redis-14154.homelab.local:14154";
+            var redisOptions = ConfigurationOptions.Parse(redisEndpoint); // edit to suit
+            redisOptions.Password = "n9ENxshPYpXRUU1lon3qP6ANDJ41iITaAICViF90FwQ="; //for ACRE connections use Access Key
+            
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisOptions);
             
             Console.WriteLine("This program will use Check and Set logic to modify a key in Redis");
@@ -28,19 +33,78 @@ namespace checkandset{
             if(null!=s){
                 newValue = s;
             }
-            Console.WriteLine("What versionID do you want to use? [use an integer]");
+            Console.WriteLine("What versionID do you want to use? [use an integer]\nThe LUA script accepts any integer value for new keys\n(and sets the versionID to 1)");
             long versionID = 0;
             s = Console.ReadLine();
             if(null!=s){
                 versionID = long.Parse(s);
             }
+            //Use System time on this client to see duration of this CheckAndSet behavior
+            Stopwatch duration = Stopwatch.StartNew();
             var newVersionID = CheckAndSetWithLuaJSON(keyName,newValue,versionID,redis); 
+            duration.Stop();
+            Console.WriteLine(">> Updating Check and Set Value took: "+duration.Elapsed); 
+            validateReturnedVersionID(versionID,newVersionID);
+
+            duration = Stopwatch.StartNew();
+            var casVal =  GetCheckAndSetJSONValue(keyName, redis);
+            duration.Stop();
+            Console.WriteLine(">> Fetching Check and Set Value took: "+duration.Elapsed); 
+            Console.WriteLine("\tAfter update -->   value of " + keyName + " = " +casVal);
             
-            Console.WriteLine("\tAfter update -->   value of " + keyName + " = " + GetCheckAndSetJSONValue(keyName, redis));
-            Console.WriteLine("\n This versionID will be expected for future mutations --> "+ newVersionID);
-            if (newVersionID != versionID + 1)
+            Console.WriteLine("\n This versionID will be expected for future mutations of your key: "+keyName+" --> "+ newVersionID);
+
+            //this faster version uses the hard-coded keyname: 'hardCodedKeyname'
+            testFasterVersion(redis);
+            Console.WriteLine("\n\n NOTE That to be even faster than this - wherever possible, operations should be placed into a pipeline and executed in a single network call");
+        }
+
+        //test faster version of method to get data:
+        // it assumes that the client code context creates a connection and reuses it
+        // this avoids wasting time recreating a connection with each call
+        // It first deletes any object with the test key name and then updates it
+        // It later fetches the new value - all of these operations use the same connection 
+        // the time taken to initialize the now shared connection is not measured
+        private static void testFasterVersion(ConnectionMultiplexer redis){
+            Console.WriteLine("\n\n************************\n\nTest of reusing RedisConnection across multiple methods ...");
+            string keyName = "hardCodedKeyname";
+            string newValue = "200987654";
+            long originalVersionID = 0;
+            long wrongVersionID = 9999;
+            string wrongValue = "999999";
+            IDatabase redisDB = redis.GetDatabase();
+            // preparation for the test - delete any old key with the hard-coded name 
+            var deleteResult = redisDB.KeyDelete(keyName);
+            Stopwatch duration = Stopwatch.StartNew();
+            var newVersionID = CheckAndSetWithLuaJSONFaster(keyName,newValue,originalVersionID,redisDB); 
+            duration.Stop();
+            validateReturnedVersionID(originalVersionID,newVersionID);
+            Console.WriteLine(">> Updating Check and Set Value took: "+duration.Elapsed); 
+            duration = Stopwatch.StartNew();
+            var casVal =  GetCheckAndSetJSONValueFaster(keyName, redisDB);
+            duration.Stop();
+            Console.WriteLine(">> Fetching Check and Set Value took: "+duration.Elapsed); 
+            Console.WriteLine("\tAfter update -->   value of " + keyName + " = " +casVal);
+            Console.WriteLine("\nDeliberately passing the wrong versionID to the CheckAndSetWithLuaJSONFaster() method.");
+            Console.WriteLine("\nTrying to set the value to: "+wrongValue);
+            duration = Stopwatch.StartNew();
+            var returnedVersionID = CheckAndSetWithLuaJSONFaster(keyName,wrongValue,wrongVersionID,redisDB); 
+            duration.Stop();
+            Console.WriteLine(">> Updating Check and Set Value took: "+duration.Elapsed); 
+            duration = Stopwatch.StartNew();
+            casVal =  GetCheckAndSetJSONValueFaster(keyName, redisDB);
+            duration.Stop();
+            Console.WriteLine(">> Fetching Check and Set Value took: "+duration.Elapsed); 
+            Console.WriteLine("\t\tAfter deliberately failed update -->   value of " + keyName + " = " +casVal);    
+            validateReturnedVersionID(wrongVersionID,returnedVersionID);
+        }
+
+
+        //Only prints a message if the versionID provided to the checkAndSet is incorrect:
+        private static void validateReturnedVersionID(long utilizedVersionID,long returnedVersionID){
+            if (returnedVersionID != utilizedVersionID + 1)
             {
-                Console.WriteLine("\t[[[[[[{ ALERT }]]]]]]\n\tValue not updated! You need to refresh your local copy - current versionID is: " + newVersionID);
+                Console.WriteLine("\t[[[[[[{ ALERT }]]]]]]\n\tValue not updated! You need to refresh your local copy - the expected versionID is: " + returnedVersionID);
             }
         }
 
@@ -50,9 +114,8 @@ namespace checkandset{
             //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisOptions);
             IDatabase redisDB = redis.GetDatabase();
             long newVersionID = 0;
-            // Create the LUA script string for our use:
-            string luaString = "local changeTime = redis.call('TIME') local putold = redis.call('JSON.RESP', KEYS[1], '$.val') local workingVersionID = redis.call('JSON.RESP', KEYS[1], '$.versionID') if not workingVersionID then workingVersionID = 1 else if workingVersionID[1]..'' == ARGV[2]..'' then workingVersionID = ARGV[2] + 1 else if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID) end end if redis.call('EXISTS', KEYS[1]) == 0 then redis.call('JSON.SET', KEYS[1], '$', '{\"val\": ' .. ARGV[1] .. '}') end if putold then redis.call('JSON.SET', KEYS[1], '$.oldval', putold[1]) end redis.call('JSON.SET', KEYS[1], '$.timestamp', changeTime[1] .. ':' .. changeTime[2]) redis.call('JSON.SET', KEYS[1], '$.versionID', workingVersionID) redis.call('JSON.SET', KEYS[1], '$.val', ARGV[1]) if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID)";
-                   // Create the List of keynames to pass along with the lua script:
+            
+            // Create the List of keynames to pass along with the lua script:
             List<RedisKey> keynamesArray = new List<RedisKey>();
             keynamesArray.Add(keyName);
             List<RedisValue> values = new List<RedisValue>();
@@ -85,6 +148,71 @@ namespace checkandset{
         private static string GetCheckAndSetJSONValue(string keyName,ConnectionMultiplexer redis){
             //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisOptions);
             IDatabase db = redis.GetDatabase();
+            IJsonCommands json = db.JSON();
+            
+            string result = "NULL Returned --This is not expected.";
+
+            RedisResult[] lResult = json.Resp(new RedisKey(keyName), "$.val");
+            if (null == lResult)
+            {
+                Console.WriteLine("Path == $.val on json object " + keyName + " is null");
+                Console.WriteLine("Exists: " + keyName + " returns: " + db.KeyExists(keyName));
+            }
+            else
+            {
+                var r = lResult[0].ToString();
+                if(null!=r){
+                    result = r;
+                }
+                Console.WriteLine("\nGetCheckAndSetJSONValue() fetches : " + result);
+            }
+            return result;
+        }
+
+// New Section of CODE where the duplicate bahavioral functions 
+// do not initiate a connection object, but reuse a presumably shared one
+
+        private static long CheckAndSetWithLuaJSONFaster(string keyName, string value, long versionID,IDatabase redisDB){
+            Console.WriteLine($"{Environment.NewLine}CheckAndSetWithLuaJSON() called with args: {keyName}, {value}, {versionID}  ...");
+            //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisOptions);
+            //IDatabase redisDB = redis.GetDatabase();
+            long newVersionID = 0;
+            // Create the LUA script string for our use:
+            //string luaString = "local changeTime = redis.call('TIME') local putold = redis.call('JSON.RESP', KEYS[1], '$.val') local workingVersionID = redis.call('JSON.RESP', KEYS[1], '$.versionID') if not workingVersionID then workingVersionID = 1 else if workingVersionID[1]..'' == ARGV[2]..'' then workingVersionID = ARGV[2] + 1 else if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID) end end if redis.call('EXISTS', KEYS[1]) == 0 then redis.call('JSON.SET', KEYS[1], '$', '{\"val\": ' .. ARGV[1] .. '}') end if putold then redis.call('JSON.SET', KEYS[1], '$.oldval', putold[1]) end redis.call('JSON.SET', KEYS[1], '$.timestamp', changeTime[1] .. ':' .. changeTime[2]) redis.call('JSON.SET', KEYS[1], '$.versionID', workingVersionID) redis.call('JSON.SET', KEYS[1], '$.val', ARGV[1]) if not tonumber(workingVersionID) then workingVersionID = workingVersionID[1] end return tonumber(workingVersionID)";
+            // Create the List of keynames to pass along with the lua script:
+            List<RedisKey> keynamesArray = new List<RedisKey>();
+            keynamesArray.Add(keyName);
+            List<RedisValue> values = new List<RedisValue>();
+            values.Add(value);
+            values.Add(versionID.ToString());
+            // Fire the lua script that adds the new value and stores any old value in the rollback attribute and retrieves the VersionID:
+            string s = "";
+            try
+            {
+                var v = redisDB.ScriptEvaluate(luaString, keynamesArray.ToArray(), values.ToArray()).ToString();
+                if(null!=v){
+                    s=v;
+                }
+                s = s.Replace('[', ' ');
+                s = s.Replace(']', ' ');
+                s = s.Trim();
+                Console.WriteLine("\n\tResponse from Check and Set lua call: " + s);
+                newVersionID = long.Parse(s);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR returned value from LUA = " + ex.Message);
+                newVersionID = -1;
+            }
+            return newVersionID;
+        }
+
+
+        // This method reuses an existing connection to Redis when it is called:
+        //for more JSON API information see: https://github.com/redis/NRedisStack/blob/master/Examples/BasicJsonExamples.md
+        private static string GetCheckAndSetJSONValueFaster(string keyName,IDatabase db){
+            //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisOptions);
+            //IDatabase db = redis.GetDatabase();
             IJsonCommands json = db.JSON();
             
             string result = "NULL Returned --This is not expected.";
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 This command line program uses LUA scripting server-side to implement check and set behavior in Redis.
 
-It also uses a JSON object as the wrapper for the protected value.
+It also uses a very simple JSON object as the wrapper for the protected value.
 ![JSON_SAMPLE](checkandsetJSONObject.png)
+
+It first asks the user to provide a keyname, an integer value, and a versionID of their choice and attempts to create the new key with the value provided while managing the incrementing of the versionID in an atomic fashion so that if the wrong versionID is provided, no change to the key occurs.
 
 To run: 
 - make sure you have dotnet 7 installed on your system to support C# execution
@@ -9,7 +11,7 @@ To run:
 ```
 dotnet add package NRedisStack
 ```
-- modify the Program.cs code so that the configuration details for Redis match your instance of Redis
+- modify the top of the Main method code in the Program.cs file (around line 11-15) so that the configuration details for Redis match your instance of Redis
 - make sure you are running a version of Redis that has the JSON module installed
 - execute 
 ```
@@ -21,41 +23,75 @@ and follow the prompts as in the following example execution:
 owentaylor@Owens-MacBook-Pro checkandset1 % dotnet run
 This program will use Check and Set logic to modify a key in Redis
 The key is of type JSON and requires that you have the JSON module installed
-A LUA script handles updating the key with a new value when the versionID is correct (in a simple, fixed path within the JSON object)
+A LUA script handles updating the key with a new value  when the versionID is correct (in a simple, fixed path within the JSON object)
 If the version ID is a mismatch, the program will not update the value in the key
 
 A Gold-plated version of this program would accept a JSON path as well as the value.
 (A future version will utilize JSON.MERGE to reduce complexity - once it is available in ACRE)
 
 What keyName do you wish to use?
-mykey1
+owenkey1
 What new Value do you wish to set? [use an integer]
-567
+8753991
 What versionID do you want to use? [use an integer]
-3
+The LUA script accepts any integer value for new keys
+(and sets the versionID to 1)
+0
 
-CheckAndSetWithLuaJSON() called with args: mykey1, 567, 3  ...
+CheckAndSetWithLuaJSON() called with args: owenkey1, 8753991, 0  ...
 
-        Response from Check and Set lua call: 4
+        Response from Check and Set lua call: 1
+>> Updating Check and Set Value took: 00:00:00.0368134
 
-getWithJedisPooled() fetches : 567
-        After update -->   value of mykey1 = 567
+GetCheckAndSetJSONValue() fetches : 8753991
+>> Fetching Check and Set Value took: 00:00:00.0314179
+        After update -->   value of owenkey1 = 8753991
 
- This versionID will be expected for future mutations --> 4
- ```
+ This versionID will be expected for future mutations of your key: owenkey1 --> 1
 
- The lua logic is somewhat dense and includes adding a timestamp and an old copy of the value of interest to be stored in the JSON object - which enables 1-level rollback behavior.
+
+************************
+
+Test of reusing RedisConnection across multiple methods ...
+
+CheckAndSetWithLuaJSON() called with args: hardCodedKeyname, 200987654, 0  ...
+
+        Response from Check and Set lua call: 1
+>> Updating Check and Set Value took: 00:00:00.0239791
+
+GetCheckAndSetJSONValue() fetches : 200987654
+>> Fetching Check and Set Value took: 00:00:00.0227882
+        After update -->   value of hardCodedKeyname = 200987654
+
+Deliberately passing the wrong versionID to the CheckAndSetWithLuaJSONFaster() method.
+
+Trying to set the value to: 999999
+
+CheckAndSetWithLuaJSON() called with args: hardCodedKeyname, 999999, 9999  ...
+
+        Response from Check and Set lua call: 1
+>> Updating Check and Set Value took: 00:00:00.0220361
+
+GetCheckAndSetJSONValue() fetches : 200987654
+>> Fetching Check and Set Value took: 00:00:00.0220558
+                After deliberately failed update -->   value of hardCodedKeyname = 200987654
+        [[[[[[{ ALERT }]]]]]]
+        Value not updated! You need to refresh your local copy - the expected versionID is: 1
+
+
+ NOTE That to be even faster than this - wherever possible, operations should be placed into a pipeline and executed in a single network call
+```
+
+ The lua logic is somewhat dense 
 
  Here is the LUA code pretty printed for humans:
-  ```
-local changeTime = redis.call('TIME')
-local putold = redis.call('JSON.RESP', KEYS[1], '$.val')
+```
 local workingVersionID = redis.call('JSON.RESP', KEYS[1], '$.versionID')
 
 if not workingVersionID then
     workingVersionID = 1
 else
-    if workingVersionID[1]..'' == ARGV[2]..'' then
+    if workingVersionID[1] .. '' == ARGV[2] .. '' then
         workingVersionID = ARGV[2] + 1
     else
         if not tonumber(workingVersionID) then
@@ -69,11 +105,6 @@ if redis.call('EXISTS', KEYS[1]) == 0 then
     redis.call('JSON.SET', KEYS[1], '$', '{\"val\": ' .. ARGV[1] .. '}')
 end
 
-if putold then
-    redis.call('JSON.SET', KEYS[1], '$.oldval', putold[1])
-end
-
-redis.call('JSON.SET', KEYS[1], '$.timestamp', changeTime[1] .. ':' .. changeTime[2])
 redis.call('JSON.SET', KEYS[1], '$.versionID', workingVersionID)
 redis.call('JSON.SET', KEYS[1], '$.val', ARGV[1])
 
